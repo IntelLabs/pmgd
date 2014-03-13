598cf6d6180306da494f492f0d9ed3fe23c7480b
Alain Kägi <alain.kagi@intel.com>
2014-03-13 16:16:03 -0700

    Add a tool to check for the absence of undesirable sequences of characters

---
+++ tools/whitespacetest.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# Must be invoked from the project's root directory.
+
+success=0           # success until otherwise detected
+
+for f in `find . -name \*.h -o -name \*.cc -o -name Makefile`; do
+    okay=0
+    msg=''
+    if echo $f | grep -qv 'Makefile$' && grep -q '	' $f; then
+        okay=1
+        msg='tabs'
+    fi
+    if grep -q '[ 	]$' $f; then
+        okay=1
+        if test -z "$msg"; then
+            msg='trailing whitespace'
+        else
+            msg="$msg, trailing whitespace"
+        fi
+    fi
+    if tail --lines=1 $f | grep -q '^[  	]*$'; then
+        okay=1
+        if test -z "$msg"; then
+            msg='trailing empty lines'
+        else
+            msg="$msg, trailing empty lines"
+        fi
+    fi
+    if grep -q $'\r$' $f; then
+        okay=1
+        if test -z "$msg"; then
+            msg='carriage returns'
+        else
+            msg="$msg, carriage returns"
+        fi
+    fi
+    if test $okay -eq 1; then
+        success=1
+        echo $f: has $msg
+    fi
+done
+
+exit $success
