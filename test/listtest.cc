c7ef9429254daeffdc5e3b16ebbce1a575368ed1
Vishakha Gupta-Cledat <vishakha.s.gupta@intel.com>
2014-03-20 15:43:32 -0700

    Fix missing transaction object problem in list and edgeindex tests
    Files affected: listtest.cc edgeindextest.cc

--- test/listtest.cc
+++ test/listtest.cc
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <string.h>
 
+#include "jarvis.h"
 #include "../src/List.h"
 #include "../src/os.h"
 #include "../src/allocator.h"
+#include "../util/util.h"
 
 using namespace Jarvis;
 using namespace std;
@@ -25,40 +27,49 @@ int main()
     cout << "List unit test\n\n";
     uint64_t start_addr;
 
-    // Need the allocator
-    struct AllocatorInfo info1;
-    struct AllocatorInfo info_arr[sizeof default_allocators];
-    bool create1 = true;
-    
-    start_addr = 0x100000000;
-    memcpy(info_arr, default_allocators, sizeof default_allocators);
-    
-    info1.offset = 0;
-    info1.len = NUM_FIXED_ALLOCATORS * REGION_SIZE;
-    info1.size = 0;
-    
-    os::MapRegion region1(".", "region1", start_addr, info1.len, create1, create1);
-    Allocator allocator1(start_addr, info_arr, NUM_FIXED_ALLOCATORS, create1);
-    
-    List<int> *list = (List<int> *)allocator1.alloc(sizeof *list);
-    cout << "Size of list obj: " << sizeof(List<int>) << "\n";
-    list->init();
+    try {
+        Graph db("listgraph", Graph::Create);
 
-    // List array elements
-    int elems[] = {7, 5, 3, 7, 4, 13, 3};
-    for (int i = 0; i < 5; ++i)
-        list->add(elems[i], allocator1);
-    cout << "Number of elems after adding: " << list->num_elems() << "\n";
-/*
-    for (int i = 0; i < 100; ++i)
-        list.add(rand() % 1000);
-        */
-    list->remove(3, allocator1);
-    for (int i = 5; i < 7; ++i)
-        list->add(elems[i], allocator1);
+        Transaction tx(db);
+        // Need the allocator
+        struct AllocatorInfo info1;
+        struct AllocatorInfo info_arr[sizeof default_allocators];
+        bool create1 = true;
 
-    for (int i = 0; i < 7; ++i)
-        list->remove(elems[i], allocator1);
-    cout << "Number of elems after removing: " << list->num_elems() << "\n";
+        start_addr = 0x100000000;
+        memcpy(info_arr, default_allocators, sizeof default_allocators);
+
+        info1.offset = 0;
+        info1.len = NUM_FIXED_ALLOCATORS * REGION_SIZE;
+        info1.size = 0;
+
+        os::MapRegion region1(".", "region1", start_addr, info1.len, create1, create1);
+        Allocator allocator1(start_addr, info_arr, NUM_FIXED_ALLOCATORS, create1);
+
+        List<int> *list = (List<int> *)allocator1.alloc(sizeof *list);
+        cout << "Size of list obj: " << sizeof(List<int>) << "\n";
+        list->init();
+
+        // List array elements
+        int elems[] = {7, 5, 3, 7, 4, 13, 3};
+        for (int i = 0; i < 5; ++i)
+            list->add(elems[i], allocator1);
+        cout << "Number of elems after adding 7,5,3,7,4: " << list->num_elems() << "\n";
+        /*
+           for (int i = 0; i < 100; ++i)
+           list.add(rand() % 1000);
+           */
+        list->remove(3, allocator1);
+        for (int i = 5; i < 7; ++i)
+            list->add(elems[i], allocator1);
+        cout << "Number of elems after removing 3, and adding 13, 3: " << list->num_elems() << "\n";
+
+        for (int i = 0; i < 7; ++i)
+            list->remove(elems[i], allocator1);
+        cout << "Number of elems after removing: " << list->num_elems() << "\n";
+    }
+    catch (Exception e) {
+        print_exception(e);
+    }
     return 0;
 }
