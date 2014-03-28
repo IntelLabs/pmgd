59201c6ede042d55ae4d55e8ad5a4734d23ead61
Philip Lantz <philip.lantz@intel.com>
2014-03-28 11:37:21 -0700

    Allow allocations of objects smaller than the smallest fixed-allocator size.
    Files affected: Allocator.cc

--- src/Allocator.cc
+++ src/Allocator.cc
@@ -9,6 +9,8 @@ using namespace Jarvis;
 
 unsigned Allocator::find_alloc_index(size_t size)
 {
+    if (size <= _fixed_allocators[0]->object_size())
+        return 0;
     unsigned index = bsr(size) - 4; // First power is 2^4 ==> first fixed alloc size
     return ( (size & (size - 1) ) ? (index + 1) : index);
 }
@@ -17,8 +19,7 @@ void *Allocator::alloc(size_t size)
 {
     // TODO This restriction will go away when we have the variable
     // allocator
-    if (size < _fixed_allocators[0]->object_size() ||
-        size > _fixed_allocators[_fixed_allocators.size() - 1]->object_size())
+    if (size > _fixed_allocators[_fixed_allocators.size() - 1]->object_size())
         throw Exception(not_implemented);
     unsigned index = find_alloc_index(size);
     return _fixed_allocators[index]->alloc();
@@ -28,8 +29,7 @@ void Allocator::free(void *addr, size_t size)
 {
     // TODO This restriction will go away when we have the variable
     // allocator
-    assert(size >= _fixed_allocators[0]->object_size() &&
-        size <= _fixed_allocators[_fixed_allocators.size() - 1]->object_size());
+    assert(size <= _fixed_allocators[_fixed_allocators.size() - 1]->object_size());
     unsigned index = find_alloc_index(size);
     return _fixed_allocators[index]->free(addr);
 }
