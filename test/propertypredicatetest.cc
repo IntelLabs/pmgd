79b8c52b9c46523e446c68f2922279ed968dcba1
Philip Lantz <philip.lantz@intel.com>
2014-04-01 14:07:22 -0700

    Change property names in propertypredicatetest.

--- test/propertypredicatetest.cc
+++ test/propertypredicatetest.cc
@@ -21,44 +21,44 @@ int main(int argc, char **argv)
             char tag[8] = {0};
             sprintf(tag, "tag%d",i);
             Node &n = db.add_node(tag);
-            n.set_property("id1", argv[i]);
-            n.set_property("id2", i);
+            n.set_property("id", argv[i]);
+            n.set_property("value", i);
         }
 
         // Look for name starting with 'a'
         printf("Nodes starting with 'a'\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id1", PropertyPredicate::gelt, "a", "b")));
+            .filter(PropertyPredicate("id", PropertyPredicate::gelt, "a", "b")));
 
         // Look for name starting with 'b'
         printf("Nodes starting with 'b'\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id1", PropertyPredicate::gelt, "b", "c")));
+            .filter(PropertyPredicate("id", PropertyPredicate::gelt, "b", "c")));
 
         // Look for value less than 2
         printf("Nodes with value less than 2\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id2", PropertyPredicate::lt, 2)));
+            .filter(PropertyPredicate("value", PropertyPredicate::lt, 2)));
 
         // Look for value less or equal to than 4
         printf("Nodes with value less than or equal to 4\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id2", PropertyPredicate::le, 4)));
+            .filter(PropertyPredicate("value", PropertyPredicate::le, 4)));
 
         // Look for value greater than 3
         printf("Nodes with value greater than 3\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id2", PropertyPredicate::gt, 3)));
+            .filter(PropertyPredicate("value", PropertyPredicate::gt, 3)));
 
         // Look for value greater than or equal to  1
         printf("Nodes with value greater than or equal to 1\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id2", PropertyPredicate::ge, 1)));
+            .filter(PropertyPredicate("value", PropertyPredicate::ge, 1)));
 
         // Look for value between 2 and 5
         printf("Nodes with value between 2 and 5\n");
         dump(db, db.get_nodes()
-            .filter(PropertyPredicate("id2", PropertyPredicate::gele, 2, 5)));
+            .filter(PropertyPredicate("value", PropertyPredicate::gele, 2, 5)));
 
         tx.commit();
     }
