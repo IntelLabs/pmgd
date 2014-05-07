4fc05eb2f9d561924ea8df38877c238362295f34
Alain Kägi <alain.kagi@intel.com>
2014-05-07 10:12:39 -0700

    Make load_tsv_test use the util library
    This commit modifies load_tsv_test found in branch
    load-tsv-properties.

---
+++ test/load_tsv_test.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "jarvis.h"
+#include "../util/util.h"
+
+using namespace Jarvis;
+
+long long num_nodes = 0;
+long long num_edges = 0;
+
+void node_added(Node &);
+void edge_added(Edge &);
+
+int main(int argc, char *argv[])
+{
+    long line = 0;
+
+    try {
+        Graph db("load_tsv_graph", Graph::Create);
+
+        load_tsv(db, stdin, node_added, edge_added);
+    }
+    catch (Exception e) {
+        std::cerr << argv[0] << ": stdin: Exception " << e.name << " occurred on line " << line << "\n";
+        return 1;
+    }
+
+    std::cout << num_nodes << "\t" << num_edges << "\n";
+
+    return 0;
+}
+
+void node_added(Node &n)
+{
+    ++num_nodes;
+}
+
+void edge_added(Edge &e)
+{
+    ++num_edges;
+}
