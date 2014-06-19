d0523aac6698d63f5de1ea34c0bb13a27e18102c
Alain Kägi <alain.kagi@intel.com>
2014-06-18 19:47:15 -0700

    Modify loadgraph.cc and dumpgraph.cc to match style used in mkgraph.cc.
    Files affected: tools/Makefile, tools/dumpgraph.cc, tools/loadgraph.cc

--- tools/dumpgraph.cc
+++ tools/dumpgraph.cc
@@ -1,17 +1,27 @@
-/*
- * This test dumps a Jarvis graph
+/**
+ * Dump the content of a graphstore to standard output
  */
 
+#include <string.h>
 #include <stdio.h>
 #include "jarvis.h"
-#include "../util/util.h"
+#include "util.h"
 
 using namespace Jarvis;
 
+void print_usage(FILE *stream);
+
 int main(int argc, char **argv)
 {
+    int argi = 1;
+
+    if (argi < argc && strcmp(argv[argi], "-h") == 0) {
+        print_usage(stdout);
+        return 0;
+    }
+
     if (argc < 2) {
-        fprintf(stderr, "Usage: dumpgraph <graph-name>\n");
+        fprintf(stderr, "dumpgraph: No graphstore specified\n");
         return 1;
     }
 
@@ -29,4 +39,12 @@ int main(int argc, char **argv)
     }
 
     return 0;
+}
+
+void print_usage(FILE *stream)
+{
+    fprintf(stream, "Usage: dumpgraph [-h] GRAPHSTORE\n");
+    fprintf(stream, "Dump the content of GRAPHSTORE.\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "  -h  print this help and exit\n");
 }
--- tools/loadgraph.cc
+++ tools/loadgraph.cc
@@ -1,37 +1,87 @@
-#include <stdio.h>
+/**
+ * Bulk load a graphstore from a file or standard input
+ */
+
 #include <string.h>
+#include <stdio.h>
 #include "jarvis.h"
-#include "../util/util.h"
+#include "util.h"
 
 using namespace Jarvis;
 
+void print_usage(FILE *stream);
+
 int main(int argc, char **argv)
 {
-    bool dump_graph = false;
+    bool tsv_format = false;
+    bool gson_format = false;
+    bool jtxt_format = false;
+    bool append = false;
     int argi = 1;
 
-    if (argi < argc && strcmp(argv[argi], "-d") == 0) {
-        dump_graph = true;
-        argi++;
+    while (argi < argc && argv[argi][0] == '-') {
+        if (strcmp(argv[argi], "-h") == 0) {
+            print_usage(stdout);
+            return 0;
+        }
+        else if (strcmp(argv[argi], "-t") == 0) {
+            tsv_format = true;
+            gson_format = jtxt_format = false;
+        }
+        else if (strcmp(argv[argi], "-g") == 0) {
+            gson_format = true;
+            tsv_format = jtxt_format = false;
+        }
+        else if (strcmp(argv[argi], "-j") == 0) {
+            jtxt_format = true;
+            tsv_format = gson_format = false;
+        }
+        else if (strcmp(argv[argi], "-a") == 0) {
+            append = true;
+        }
+        else {
+            fprintf(stderr, "loadgraph: %s: Unrecognized option\n", argv[argi]);
+            return 1;
+        }
+        argi += 1;
     }
 
-    if (argi + 2 > argc) {
-        fprintf(stderr, "Usage: loadgraph [-d] <graph-name> <file-name>\n");
+    if (argi + 1 > argc) {
+        fprintf(stderr, "loadgraph: No graphstore specified\n");
         return 1;
     }
 
     const char *db_name = argv[argi];
-    const char *file_name = argv[argi+1];
+    argi += 1;
 
+    const char *file_name = (argi < argc) ? argv[argi] : "-";
+
+    if (!tsv_format && !gson_format && !jtxt_format) {
+        const size_t len = strlen(file_name);
+
+        if (strcmp(file_name + len - 5, ".gson") == 0)
+            gson_format = true;
+        else if (strcmp(file_name + len - 5, ".jtxt") == 0)
+            jtxt_format = true;
+    }
+            
     try {
-        Graph db(db_name, Graph::Create);
-        load(db, file_name);
+        Graph db(db_name);
 
-        if (dump_graph) {
+        if (!append) {
             Transaction tx(db);
-            dump_nodes(db);
-            dump_edges(db);
+            if (db.get_nodes()) {
+                fprintf(stderr, "loadgraph: Graphstore is not empty\n");
+                return 1;
+            }
         }
+
+        if (gson_format)
+            load_gson(db, file_name);
+        else if (jtxt_format)
+            load(db, file_name);
+        else
+            load_tsv(db, file_name);
     }
     catch (Exception e) {
         print_exception(e);
@@ -39,4 +89,34 @@ int main(int argc, char **argv)
     }
 
     return 0;
+}
+
+void print_usage(FILE *stream)
+{
+    fprintf(stream, "Usage: loadgraph [OPTION]... GRAPHSTORE [FILE]\n");
+    fprintf(stream, "Bulk load a GRAPHSTORE from a FILE or standard input.\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "  -h  print this help and exit\n");
+    fprintf(stream, "  -t  input is tab-separated integers (default)\n");
+    fprintf(stream, "  -g  input is GraphSON\n");
+    fprintf(stream, "  -j  input is the Jarvis Lake graph text format\n");
+    fprintf(stream, "  -a  load graph into a non-empty graphstore\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "GRAPHSTORE must already exist (e.g., using mkgraph) and is expected to\n");
+    fprintf(stream, "be empty.  The loader will fail with a warning if GRAPHSTORE is not\n");
+    fprintf(stream, "empty.  The user can force the loading a graph in a non-empty\n");
+    fprintf(stream, "GRAPHSTORE specifying the -a option.\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "The graph loader reads its input from FILE, if provided, or standard\n");
+    fprintf(stream, "input otherwise.  The graph loader supports three input formats:\n");
+    fprintf(stream, "tab-separated integers (file extension 'tsv'), GraphSON (file extension\n");
+    fprintf(stream, "'gson'), or the Jarvis Lake graph text format (file extension 'jtxt').\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "The graph loader attempts to infer the input format based on the\n");
+    fprintf(stream, "extension of the supplied file.  Failing that it relies on the user\n");
+    fprintf(stream, "specifying the format with the appropriate option.  Options override\n");
+    fprintf(stream, "inferences based on the file extension.\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "To speed up processing, loadgraph will create a node index on property\n");
+    fprintf(stream, "identifer 'jarvis.loader.id' associated with tag '0'.\n");
 }
