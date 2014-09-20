d9da0633e97cb529adf864395893e0afb9bf2d98
Philip Lantz <philip.lantz@intel.com>
2014-09-19 18:45:26 -0700

    Check for null iterator in NodeIterator and throw exception.

--- bindings/NodeIterator.java
+++ bindings/NodeIterator.java
@@ -22,31 +22,43 @@ public class NodeIterator {
 
 
 
+    private void null_check(String method) throws Exception
+    {
+        if (current == null)
+            throw new Exception(2, "null_iterator",
+                                "NodeIterator." + method, null);
+    }
+
     // Wrap the functions for node, so we can act on the current
     // Because java doesn't allow overriding operators, we cannot
     //  just let them slide through to jarvis.
-    public Node get_current()
+    public Node get_current() throws Exception
     {
+        null_check("get_current");
         return current;
     }
 
     public String get_tag() throws Exception
     {
+        null_check("get_tag");
         return current.get_tag();
     }
 
     public Property get_property(String property) throws Exception
     {
+        null_check("get_property");
         return current.get_property(property);
     }
 
     public void set_property(String id, Property prop) throws Exception
     {
+        null_check("set_property");
         current.set_property(id, prop);
     }
 
     public void remove_property(String name) throws Exception
     {
+        null_check("remove_property");
         current.remove_property(name);
     }
 
