IntelLabs/pmgd

commit 09004dde569b6967ba27d8b24da62fed3c51160b

Change mechanism to mark transaction as committed. (Mantis #750)

src/graph.cc
@@ -21,7 +21,7 @@ static constexpr char info_name[] = "graph.jdb";
 extern constexpr char commit_id[] = "Commit id: " COMMIT_ID;
 
 struct GraphImpl::GraphInfo {
-    static const uint64_t VERSION = 2;
+    static const uint64_t VERSION = 3;
 
     uint64_t version;
 
@@ -36,6 +36,8 @@ struct GraphImpl::GraphInfo {
     uint32_t max_stringid_length;
 
     char locale_name[32];
+
+    uint64_t last_transaction_id;
 
     uint32_t num_fixed_allocators;
     uint64_t allocator_offsets[];
@@ -149,7 +151,8 @@ GraphImpl::GraphImpl(const char *name, int options, const Graph::Config *config)
       _node_region(name, _init.info->node_info, _init.create, _init.read_only),
       _edge_region(name, _init.info->edge_info, _init.create, _init.read_only),
       _allocator_region(name, _init.info->allocator_info, _init.create, _init.read_only),
-      _transaction_manager(_init.info->transaction_info.addr,
+      _transaction_manager(&_init.info->last_transaction_id,
+                           _init.info->transaction_info.addr,
                            _init.info->transaction_info.len,
                            _init.info->journal_info.addr,
                            _init.info->journal_info.len,

src/transaction.cc
@@ -33,7 +33,6 @@ void Transaction::commit()
 
 // 64B Journal entries
 const static uint8_t JE_MAX_LEN = 48;
-const static uint8_t JE_COMMIT_MARKER = 0x77;
 
 struct TransactionImpl::JournalEntry {
     TransactionId tx_id;
@@ -78,10 +77,9 @@ TransactionImpl::~TransactionImpl()
         } else {
             rollback(_tx_handle, _jcur);
         }
-        release_locks();
-
         TransactionManager *tx_manager = &_db->transaction_manager();
         tx_manager->free_transaction(_tx_handle);
+        release_locks();
     }
 
     _per_thread_tx = _outer_tx;
@@ -130,11 +128,10 @@ void TransactionImpl::log_je(void *src_ptr, size_t len)
     _jcur++;
 }
 
-// The last record in the journal is fixed as the COMMIT record.
 void TransactionImpl::log(void *ptr, size_t len)
 {
     assert(len > 0);
-    size_t je_entries = (len + JE_MAX_LEN - 1) / JE_MAX_LEN;
+    size_t je_entries = (len + JE_MAX_LEN) / JE_MAX_LEN;
 
     if (_jcur + je_entries >= jend()) {
         if (_tx_type & Transaction::ReadWrite)
@@ -161,13 +158,6 @@ void TransactionImpl::finalize_commit()
     for (JournalEntry *je = jbegin(); je < _jcur; je++)
         clflush(je->addr);
     persistent_barrier(34);
-
-    // Log the commit record
-    JournalEntry *jcommit = jend() - 1;
-    jcommit->type = JE_COMMIT_MARKER;
-    jcommit->tx_id = tx_id();
-    clflush(jcommit);
-    persistent_barrier(37);
 }
 
 
@@ -220,17 +210,10 @@ void TransactionImpl::flush_range(void *ptr, size_t len)
 
 // static function to allow recovery from TransactionManager.
 // There are no real TransactionImpl objects at recovery time.
-// The last record in the journal is fixed as the COMMIT record.
 void TransactionImpl::recover_tx(const TransactionHandle &h)
 {
-    TransactionId tx_id = h.id;
     JournalEntry *jend = static_cast<JournalEntry *>(h.jend);
-    JournalEntry *jcommit = jend - 1;
-
-    if (!(jcommit->tx_id == tx_id && jcommit->type == JE_COMMIT_MARKER)) {
-        // COMMIT record not found. Rollback !
-        rollback(h, jcommit);
-    }
+    rollback(h, jend);
 }
 
 void TransactionImpl::rollback(const TransactionHandle &h,

src/TransactionManager.cc
@@ -8,10 +8,12 @@
 using namespace Jarvis;
 
 TransactionManager::TransactionManager(
+            uint64_t *tx_id,
             uint64_t transaction_table_addr, uint64_t transaction_table_size,
             uint64_t journal_addr, uint64_t journal_size,
             bool create, bool read_only)
-    : _tx_table(reinterpret_cast<TransactionHdr *>(transaction_table_addr)),
+    : _cur_tx_id(tx_id),
+      _tx_table(reinterpret_cast<TransactionHdr *>(transaction_table_addr)),
       _journal_addr(reinterpret_cast<void *>(journal_addr)),
       _max_transactions(transaction_table_size / sizeof (TransactionHdr)),
       _extent_size(journal_size / _max_transactions),
@@ -26,14 +28,13 @@ TransactionManager::TransactionManager(
     if (_max_extents < _max_transactions)
         throw Exception(invalid_config);
 
-    if (create)
+    if (create) {
         reset_table();
-    else if (!read_only)
+        *_cur_tx_id = 0;
+    } else if (!read_only)
         recover();
     else
         check_clean();
-
-    _cur_tx_id = 0;
 }
 
 void *TransactionManager::tx_jbegin(int index)
@@ -97,13 +98,13 @@ TransactionHandle TransactionManager::alloc_transaction(bool read_only)
         return TransactionHandle(-1, -1, dummy, dummy);
     }
 
-    uint32_t tx_id = atomic_inc(_cur_tx_id) + 1;
+    TransactionId tx_id = atomic_inc(*_cur_tx_id) + 1;
+    clflush(_cur_tx_id);
 
     for (int i = 0; i < _max_transactions; i++) {
         TransactionHdr *hdr = &_tx_table[i];
-        if (hdr->tx_id == 0 && cmpxchg(hdr->tx_id, 0u, tx_id)) {
+        if (hdr->tx_id == 0 && cmpxchg(hdr->tx_id, (TransactionId)0, tx_id)) {
             clflush(hdr);
-            persistent_barrier(21); // Is this required?
             return TransactionHandle(tx_id, i, tx_jbegin(i), tx_jend(i));
         }
     }
@@ -115,9 +116,10 @@ void TransactionManager::free_transaction(const TransactionHandle &handle)
     // If handle.index is -1, this is a read-only transaction, and
     // nothing needs to be done.
     if (handle.index != -1) {
+        // Writing 0 to the transaction-id commits the transaction
         TransactionHdr *hdr = &_tx_table[handle.index];
         hdr->tx_id = 0;
         clflush(hdr);
-        persistent_barrier(24); // Is this required?
+        persistent_barrier(24);
     }
 }

src/TransactionManager.h
@@ -4,7 +4,10 @@
 #include <stdint.h>
 
 namespace Jarvis {
-    typedef uint32_t TransactionId;
+    // TransactionId is never reset and should not roll-over.
+    // A 64-bit transaction ID supports 3 billion transactions
+    // per second for 100 years.
+    typedef uint64_t TransactionId;
 
     struct TransactionHandle {
         TransactionId id;
@@ -27,7 +30,8 @@ namespace Jarvis {
     };
 
     class TransactionManager {
-        TransactionId _cur_tx_id;
+        // Transaction ID in PM
+        TransactionId *_cur_tx_id;
 
         // Transaction table in PM
         TransactionHdr *_tx_table;
@@ -46,7 +50,8 @@ namespace Jarvis {
         void *tx_jend(int index);
 
     public:
-        TransactionManager(uint64_t transaction_table_addr,
+        TransactionManager(uint64_t *tx_id,
+                           uint64_t transaction_table_addr,
                            uint64_t transaction_table_size,
                            uint64_t journal_addr,
                            uint64_t journal_size,

commit 784ca78a7f7fcfd4ed1c6658d0c4317cc675e90f

Fix journal entry size, broken since commit 09004dd. Graph version 4.
Files affected: transaction.cc, graph.cc

src/graph.cc
@@ -21,7 +21,7 @@ static constexpr char info_name[] = "graph.jdb";
 extern constexpr char commit_id[] = "Commit id: " COMMIT_ID;
 
 struct GraphImpl::GraphInfo {
-    static const uint64_t VERSION = 3;
+    static const uint64_t VERSION = 4;
 
     uint64_t version;
 

src/transaction.cc
@@ -35,11 +35,8 @@ void Transaction::commit()
 const static uint8_t JE_MAX_LEN = 48;
 
 struct TransactionImpl::JournalEntry {
-    TransactionId tx_id;
-    union {
-        uint8_t len;
-        uint8_t type;
-    };
+    TransactionId tx_id : 56;
+    uint8_t len : 8;
     void *addr;
     uint8_t data[JE_MAX_LEN];
 };
@@ -52,6 +49,8 @@ TransactionImpl::TransactionImpl(GraphImpl *db, int options)
       _committed(false),
       _commit_callback_list(NULL)
 {
+    static_assert(sizeof (TransactionImpl::JournalEntry) == 64, "Journal entry size is not 64 bytes.");
+
     bool read_write = _tx_type & Transaction::ReadWrite;
 
     if (read_write)