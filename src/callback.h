54a8d2cebbe170acbebe45a17b8ccefd98b020f2
Philip Lantz <philip.lantz@intel.com>
2015-08-12 16:31:15 -0700

    Add callback.h.

---
+++ src/callback.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <list>
+#include <functional>
+
+namespace Jarvis {
+    template <typename O, typename P> class CallbackList {
+        typedef std::function<void(P)> F;
+        struct Callback { O key; F f; };
+
+        std::list<Callback> _list;
+
+    public:
+        void register_callback(O key, F f)
+            { _list.push_back(Callback{key, f}); }
+
+        void unregister_callback(O key)
+            { _list.remove_if([key](Callback &p) { return p.key == key; }); }
+
+        void do_callbacks(P p) const
+        {
+            for (auto cb : _list)
+                cb.f(p);
+        }
+    };
+
+    template <typename O> class CallbackList<O, void> {
+        typedef std::function<void()> F;
+        struct Callback { O key; F f; };
+
+        std::list<Callback> _list;
+
+    public:
+        void register_callback(O key, F f)
+            { _list.push_back(Callback{key, f}); }
+
+        void unregister_callback(O key)
+            { _list.remove_if([key](Callback &p) { return p.key == key; }); }
+
+        void do_callbacks() const
+        {
+            for (auto cb : _list)
+                cb.f();
+        }
+    };
+
+    template <typename P> class CallbackList<void, P> {
+        typedef std::function<void(P)> F;
+        struct Callback { F f; };
+
+        std::list<Callback> _list;
+
+    public:
+        void register_callback(F f) { _list.push_back(Callback{f}); }
+
+        void do_callbacks(P p) const
+        {
+            for (auto cb : _list)
+                cb.f(p);
+        }
+    };
+
+    template <> class CallbackList<void, void> {
+        typedef std::function<void()> F;
+        struct Callback { F f; };
+
+        std::list<Callback> _list;
+
+    public:
+        void register_callback(F f) { _list.push_back(Callback{f}); }
+
+        void do_callbacks() const
+        {
+            for (auto cb : _list)
+                cb.f();
+        }
+    };
+};
