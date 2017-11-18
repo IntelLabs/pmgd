a7275bc9b2f3803eaf5996dc9f69a9dd45db3bbd
Vishakha Gupta-Cledat <vishakha.s.gupta@intel.com>
2017-11-17 16:44:10 -0800

    Add an allocator abort testing unit test.
    Add the test to run_all.sh.
    
    Files affected: allocaborttest.cc, Makeconf, Allocator.h,
                    run_all.sh, VariableAllocator.cc

--- test/run_all.sh
+++ test/run_all.sh
@@ -74,7 +74,7 @@ mkdir -p log
 # While this list could be constructed dynamically, keep it static
 # to allow for choosing and making sure we don't delete wrong dirs
 # by mistake.
-tests=( alloctest avltest chunklisttest edgeindextest
+tests=( alloctest allocaborttest avltest chunklisttest edgeindextest
         emailindextest filtertest indextest indexstringtest
         indexrangetest listtest load_gson_test load_tsv_test
         neighbortest nodeedgetest propertychunktest propertypredicatetest
@@ -87,6 +87,7 @@ tests=( alloctest avltest chunklisttest edgeindextest
         BindingsTest DateTest )
 
 graph_dirs=( fixedallocgraph varallocgraph avlgraph chunklistgraph edgeindexgraph
+             fixedallocabortgraph varallocabortgraph varallocabortlargegraph
              emailindexgraph filtergraph indexgraph indexstringgraph
              indexrangegraph listgraph load_gson_graph load_tsv_graph
              neighborgraph nodeedgegraph propertychunkgraph ppgraph
