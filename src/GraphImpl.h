cfcafc531fabc99e3a76aa488c915b9c50980f94
Vishakha Gupta-Cledat <vishakha.s.gupta@intel.com>
2017-12-20 11:18:49 -0800

    Add locking to node and edge calls.
    Files affected: node.cc, edge.cc, edge.h, graph.cc, GraphImpl.h

--- src/GraphImpl.h
+++ src/GraphImpl.h
@@ -44,9 +44,11 @@ namespace PMGD {
     struct RegionInfo;
 
     class GraphImpl {
+    public:
         typedef FixedAllocator NodeTable;
         typedef FixedAllocator EdgeTable;
 
+    private:
         struct GraphInfo;
 
         struct GraphInit {
