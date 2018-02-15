15395e36fe5ce0cf73c83d06b5e8d8798b867e46
Vishakha Gupta-Cledat <vishakha.s.gupta@intel.com>
2018-02-14 16:08:15 -0800

    Add LICENSE file and headers

--- test/alloctest.cc
+++ test/alloctest.cc
@@ -1,3 +1,32 @@
+/**
+ * @file   alloctest.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2017 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 /*
  * This unit test exercises the fixed-size allocator.
  */
@@ -483,7 +512,6 @@ void var_large_tests(Graph &db, Allocator &allocator1)
     printf("Now allocate again, but just the smaller portion\n");
     addr = allocator1.alloc(size1M * 0.5);
     passfail(testnum++, base, (long)addr);
-    
     tx1.commit();
 
     // Also, there will be no header for this one.
