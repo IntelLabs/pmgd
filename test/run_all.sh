e20f06543fdb21e214afe0ed76c4a37208d10c4e
Vishakha Gupta-Cledat <vishakha.s.gupta@intel.com>
2018-06-11 14:40:00 -0700

    Add concurrent add, find, remove tests for nodes/edges.
    Files affected: test/Makeconf, mtaddfindremovetest.cc, run_all.sh

--- test/run_all.sh
+++ test/run_all.sh
@@ -77,7 +77,7 @@ tests=( alloctest allocaborttest avltest chunklisttest edgeindextest
         reverseindexrangetest rotest
         statsindextest statsallocatortest
         soltest stringtabletest txtest removetest
-        mtalloctest stripelocktest mtavltest
+        mtalloctest stripelocktest mtavltest mtaddfindremovetest
         test720 test750 test767
         load_pmgd_tests
         BindingsTest DateTest )
@@ -91,7 +91,7 @@ graph_dirs=( fixedallocgraph varallocgraph avlgraph chunklistgraph edgeindexgrap
              statsindexgraph statsallocatorgraph
              reverseindexrangegraph rograph
              solgraph stringtablegraph txgraph removegraph
-             mtallocgraph
+             mtallocgraph mtaddfindremovegraph
              test720graph test750graph test767graph
              bindingsgraph )
 
